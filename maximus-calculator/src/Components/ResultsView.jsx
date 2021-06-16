dfbe9006592a2cac177b40649b2bedb6354a40f7
Chris Gray <combly123@gmail.com>
2021-06-15 21:07:47 -0400

    added basic conditional rendering

--- maximus-calculator/src/Components/ResultsView.jsx
+++ maximus-calculator/src/Components/ResultsView.jsx
@@ -11,8 +11,8 @@ function ResultsView(props)
         <div>
             <Typography>To hit: {attack.ToHit}</Typography>
             <Typography>Bludgeoning: {attack.Bludgeoning}</Typography>
-            <Typography>Force: {attack.Force}</Typography>
-            <Typography>Radiant: {attack.Radiant}</Typography>
+            {attack.Force > 0 && <Typography>Force: {attack.Force}</Typography>}            
+            {attack.Radiant > 0 && <Typography>Radiant: {attack.Radiant}</Typography>}
             <Typography>Total: {attack.TotalDamage}</Typography>            
         </div>
     )
